1d0a3747a539578c733d70c5dc429ef954d86849
Ben Best <ben@ecoquants.com>
2023-01-25 11:50:07 -0800

    yay! logging q&a into gsheet, locally

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # To run locally:
 #   Terminal: uvicorn main:app --reload
 #   Browser: http://127.0.0.1:8000.
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
@@ -12,6 +12,7 @@ from dotenv import load_dotenv
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from datetime import datetime
 
 app = FastAPI()
 
@@ -30,15 +31,47 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # load Google Service Account Key and Scopes
-GKEY_JSON = os.getenv("PATH_GOOGLE-SA_KEY_JSON")
+GKEY_JSON = os.getenv("PATH_GOOGLE_SA_KEY_JSON")
 GSCOPES = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"]
+# [q&a - cstories.app/beta - Google Sheet](https://docs.google.com/spreadsheets/d/1-Httr306NaDwIIt8WKvYewKvJ2K_oEGh8FonJZpFLHA/edit#gid=0)
+GSHEET_ID = '1-Httr306NaDwIIt8WKvYewKvJ2K_oEGh8FonJZpFLHA'
+GSHEET_RANGE = 'data!A2:D3'
+
+def add_q_a(q, a, client_ip):
+  """Add question & answer to Google Sheet
+  appends to the sheet
+  """
+  # TODO: add time to process the question
+
+  try:
+    creds = service_account.Credentials.from_service_account_file(
+      GKEY_JSON).with_scopes(GSCOPES)
+    service = build(
+      'sheets', 'v4', credentials=creds)
+
+    # TODO: sanitize q & a from quotes, etc
+    data = {
+      'values' : [[
+        client_ip, 
+        datetime.now().strftime("%Y-%d-%m %H:%M:%S"), 
+        q, 
+        a]] }
+
+    response = service.spreadsheets().values().append(
+      spreadsheetId    = GSHEET_ID, 
+      body             = data, 
+      range            = 'A1:D2', 
+      valueInputOption = 'USER_ENTERED').execute()
+    
+  except HttpError as err:
+    print(err)
 
 @app.get("/")
 async def root():
     return RedirectResponse(url="/docs")
 
 @app.get("/answer")
-def answer(question: str):
+def answer(question: str, request: Request):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=question,
@@ -48,4 +81,6 @@ def answer(question: str):
         frequency_penalty=0,
         presence_penalty=0,
     )
-    return response.choices[0].text
+    answer = response.choices[0].text
+    add_q_a(question, answer, request.client.host)
+    return answer
